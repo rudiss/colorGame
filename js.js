79e7ad0e19b7477b19dea709bd2857ab1a37012f
Rudinei Silva <rudislpp@gmail.com>
2017-10-20 01:40:04 -0200

    starting game logic

--- js.js
+++ js.js
@@ -8,7 +8,24 @@ var colors = [
 ];
 
 var squares = document.querySelectorAll(".square");
+var pickedColor = colors[3];
+var colorDisplay = document.getElementById('rgb')
 
-for(var i = 0; i < squares.length; i++){
+colorDisplay.textContent = pickedColor;
+
+for (var i = 0; i < squares.length; i++) {
+  //add initial colors to squares
   squares[i].style.backgroundColor = colors[i];
-}
+
+  //add click listeners to squares
+  squares[i].addEventListener("click", function() {
+    //grab color to pickedColor
+    var clickedColor = this.style.backgroundColor;
+    //compare color to pickedColor
+    if (clickedColor === pickedColor) {
+      alert("Correct!");
+    } else {
+      alert("WRONG!!")
+    }
+  });
+};
