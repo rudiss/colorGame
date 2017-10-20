502d7817d3c1440ba98d00513ad1b3bb7b282a0b
Rudinei Silva <rudislpp@gmail.com>
2017-10-20 02:25:58 -0200

    pickColor

--- js.js
+++ js.js
@@ -1,15 +1,16 @@
 var colors = [
-  "rgb(255,0,0)",
-  "rgb(255,255,0)",
-  "rgb(0,255,0)",
-  "rgb(0,255,255)",
-  "rgb(0,0,255)",
-  "rgb(255,0,255)",
+"rgb(255, 0, 0)",
+"rgb(255, 255, 0)",
+"rgb(0, 255, 0)",
+"rgb(0, 255, 255)",
+"rgb(0, 0, 255)",
+"rgb(255, 0, 255)"
 ];
 
 var squares = document.querySelectorAll(".square");
-var pickedColor = colors[3];
-var colorDisplay = document.getElementById('rgb')
+var pickedColor = pickColor();
+var colorDisplay = document.getElementById("colorDisplay");
+var messageDisplay = document.getElementById("message");
 
 colorDisplay.textContent = pickedColor;
 
@@ -23,9 +24,23 @@ for (var i = 0; i < squares.length; i++) {
     var clickedColor = this.style.backgroundColor;
     //compare color to pickedColor
     if (clickedColor === pickedColor) {
-      alert("Correct!");
+      messageDisplay.textContent = "Corret well done!";
+      changeColors(clickedColor);
     } else {
-      alert("WRONG!!")
+      this.style.backgroundColor = "#232323";
+      messageDisplay.textContent = "Try Again";
     }
   });
-};
+}
+
+function changeColors(color) {
+  //loop througth all squares
+  for (var i = 0; i < squares.length; i++) {
+    //change each color to match given color
+    squares[i].style.backgroundColor = color;
+  };
+}
+function pickColor() {
+  var random = Math.floor(Math.random() * colors.length);
+  return colors[random];
+}
